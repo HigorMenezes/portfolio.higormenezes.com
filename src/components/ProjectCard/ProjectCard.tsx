20a2fd0aa7db440344b2ace88d122ddcede29c17
Higor Menezes <higormenezes1996@gmail.com>
2020-12-15 22:37:16 -0300

    added responsivity to project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -3,28 +3,22 @@ import styles from "./styles.module.scss";
 function ProjectCard(): JSX.Element {
   return (
     <section className={styles.root}>
-      <div className={styles.about}>
-        <div>
-          <h3 className={styles.title}>Rich Text Editor</h3>
-          <p className={styles.description}>
-            A front-end project to edit text.
-          </p>
-        </div>
-        <div className={styles.techs}>
-          <img
-            className={styles.techImage}
-            title="React"
-            src="/icons/tech/reactjs.svg"
-            alt="ReactJS Logo"
-          />
-        </div>
-      </div>
+      <h3 className={styles.title}>Rich Text Editor</h3>
       <img
         className={styles.projectImage}
         title="Rich Text Editor"
         src="/projects/rich-text-editor/1.png"
         alt="Rich Text Editor Interface"
       />
+      <p className={styles.description}>A front-end project to edit text.</p>
+      <div className={styles.techs}>
+        <img
+          className={styles.techImage}
+          title="React"
+          src="/icons/tech/reactjs.svg"
+          alt="ReactJS Logo"
+        />
+      </div>
     </section>
   );
 }
